2e4ac6c2ed6788aca27ec8c0de5f7d08b9bf43de
David Oyinbo <dev.davexoyinbo@gmail.com>
2026-02-19 16:45:14 +0100

    feat: eslint and combined workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,11 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    ignores: [
+      '.nuxt/**',
+      '.output/**',
+      'node_modules/**'
+    ]
+  }
 )
